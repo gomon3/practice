ca96c23358453627b3ed589c8d8cf2bd9f9581b6
Brandon <bran_gomon25@hotmail.com>
2019-01-03 07:14:48 -0600

    Herramienta para agregar html al text area de soluciones

--- resources/views/administration/solution/create.blade.php
+++ resources/views/administration/solution/create.blade.php
@@ -26,6 +26,40 @@
 
                     <button type="submit" class="btn btn-primary">Guardar</button>
                 </form>
+
+
+                <br>
+                <br>
+
+                <p>Si deseas agregar links, llena lo siguientes campos, recuerda que en el campo URL, debes empezar con http:// o https://</p>
+                <div class="form-inline">
+                    <div class="form-group">
+                        <label for="url">URL: </label>
+                        <input type="text" class="form-control" id="url" placeholder="https://www.facebook.com">
+                    </div>
+                    <div class="form-group">
+                        <label for="remplazo">Texto: </label>
+                        <input type="text" class="form-control" id="remplazo" placeholder="Facebook">
+                    </div>
+                    <button type="button" class="btn btn-primary" onclick="insert()">Agregar</button>
+                </div>
+
+                <script>
+                    function insert()
+                    {
+                        var url = $('#url').val();
+                        var texto = $('#remplazo').val();
+
+                        var html_tag = '<a href="' + url + '">' + texto + '</a>';
+
+
+                        $('#url').val('');
+                        $('#remplazo').val('');
+
+                        $('#information').val($('#information').val() + ' ' + html_tag);
+                    }
+                </script>
+
             </div>
             
         </div>
